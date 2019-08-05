ff6ba7c424cc3a60970bd7867f5dfa983dbb545f
Leif Guillermo <guillermoleif@gmail.com>
2019-08-04 22:48:02 -0600

    Add some ticket service data structures. They haven't been tested yet.

--- src/app/ticket/ticket.service.ts
+++ src/app/ticket/ticket.service.ts
@@ -4,6 +4,31 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TicketService {
+  private ticketHistory: { ticketNumberHistory: TicketNumberHistory; ticketDateHistory: TicketDateHistory };
 
-  constructor() { }
+  constructor() {}
+
+  public addTicketToHistory(ticket: Ticket, date: Date) {
+    if (!this.ticketHistory) {
+      this.ticketHistory = {
+        ticketNumberHistory: [ticket.ticketNumber][date.toString()],
+        ticketDateHistory: [date.toString()][ticket.ticketNumber]
+      };
+    }
+    this.ticketHistory.ticketNumberHistory[ticket.ticketNumber][date.toString()].push(ticket);
+    this.ticketHistory.ticketDateHistory[date.toString()][ticket.ticketNumber] = ticket;
+  }
+}
+
+interface TicketNumberHistory {
+  [ticketNumber: string]: { [dateString: string]: Ticket[] };
+}
+
+interface TicketDateHistory {
+  [dateString: string]: { [ticketNumber: string]: Ticket };
+}
+
+export interface Ticket {
+  ticketNumber: string;
+  comments: string;
 }
